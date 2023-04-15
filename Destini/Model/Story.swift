9ab19cd4d751fa329a813aa347bc73127d3436c9
Евгений Житников <ekzhitnikov@gmail.com>
2023-04-15 11:33:15 +0400

    final

---
+++ Destini/Model/Story.swift
@@ -0,0 +1,18 @@
+//
+//  Story.swift
+//  Destini
+//
+//  Created by Евгений Житников on 15.04.2023.
+//
+
+import Foundation
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
